00a132a61643aee4ade8204a05dcc1d33d2f9756
Dirk Schut <dschut@outlook.com>
2020-07-06 00:33:49 +0200

    Move camera and player to GPS location.

---
+++ GPS Game/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour
+{
+    public GPSManager GPSManager;
+    public Camera camera;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GPSManager.IsReady)
+        {
+            Vector3 camPos = camera.transform.position;
+            camPos.x = GPSManager.position.x;
+            camPos.z = GPSManager.position.y;
+            camera.transform.position = camPos;
+
+            Vector3 playerPos = player.transform.position;
+            playerPos.x = GPSManager.position.x;
+            playerPos.z = GPSManager.position.y;
+            player.transform.position = playerPos;
+        }
+    }
+}
