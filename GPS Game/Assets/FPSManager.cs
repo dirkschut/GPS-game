1e432dfbf84fec4cc33c0c0178d389a600f65665
Dirk Schut <dschut@outlook.com>
2020-07-09 16:30:19 +0200

    Added FPS counter #23

---
+++ GPS Game/Assets/FPSManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSManager : MonoBehaviour
+{
+    private float interval = 1;
+    private float totalDelta = 0;
+    private int frameCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalDelta += Time.deltaTime;
+        frameCounter++;
+
+        if(totalDelta >= interval)
+        {
+            float fps = frameCounter / totalDelta;
+            GetComponent<TMPro.TextMeshProUGUI>().text = Mathf.Floor(fps).ToString() + " FPS";
+            frameCounter = 0;
+            totalDelta = 0f;
+        }
+        
+    }
+}
