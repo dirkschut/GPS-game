58c857202e36782e760be15cc3dcf5ecf7d160ad
Dirk Schut <dschut@outlook.com>
2020-07-14 13:39:18 +0200

    #46 Added discord server and button

---
+++ GPS Game/Assets/Scripts/URLButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLButtonManager : MonoBehaviour
+{
+    /// <summary>
+    /// Opens the given URL.
+    /// </summary>
+    /// <param name="URL">THe URL to open</param>
+    public void OnClick(string URL)
+    {
+        Application.OpenURL(URL);
+    }
+}
