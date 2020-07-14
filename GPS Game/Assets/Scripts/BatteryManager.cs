26c42cf3d125f0752e93e112eba4fbcd22d00f62
Dirk Schut <dschut@outlook.com>
2020-07-14 18:44:42 +0200

    #40 SHow battery percentage on GUI

---
+++ GPS Game/Assets/Scripts/BatteryManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float battery = SystemInfo.batteryLevel;
+        battery = Mathf.Clamp(battery, 0f, 100f);
+        GetComponent<TMPro.TextMeshProUGUI>().text = "Battery: \n" + battery.ToString("P0");
+    }
+}
